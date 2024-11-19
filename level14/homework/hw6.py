cbc40c022ad4b014abd8deb1f88102232a88b428
JUJI18 <tornikekhurcia5@gmail.com>
2024-11-19 06:12:37 -0800

    twelfth commit

---
+++ level14/homework/hw6.py
@@ -0,0 +1,21 @@
+#6)
+#შეამოწმეთ შეყვანილი სიტყვა hello-ა თუ hello world
+
+
+welcome = input("type word:")
+
+print(welcome == "hello" or welcome == "hello world")
+
+
+if welcome == "hello":
+    print("Hi🙌")
+else:
+    print("yeah world is beautiful")
+
+
+if welcome == "hello world":
+    print("hi from world🌎")
+else:
+    print("welcome🐰")
+
+

88a4bf85f3c676c0d9a01655fbe03f0ecafe7c2b
JUJI18 <tornikekhurcia5@gmail.com>
2024-11-26 07:53:05 -0800

    Fifteenth commit

---
+++ level16/homework/hw7.py
@@ -0,0 +1,10 @@
+#7) დედამ გაგიშვათ მაღაზიაში რადგან გენახათ მათ ჰქონდათ თუ არა მინერალური წყალი. 
+# თუ მაღაზიას აქვს მინერალური წყალი, დედას უთხარით რომ მაღაზიაში მინერალური წყალი აქვთ, თუ არ უდევთ დახლებზე მაშინ უთხარით რომ აღარ აქვთ. 
+
+shop = input("Do you have mineral water?")
+if shop== "no":
+    print("Mom, there is no mineral water in the shop!")
+elif shop == "yes":
+    print("Mom, there is mineral water in shop!")
+
+

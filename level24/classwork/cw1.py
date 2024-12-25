63b9bf8efc8a51a46241fe0360c5f1ac68ee094f
JUJI18 <tornikekhurcia5@gmail.com>
2024-12-25 06:32:14 -0800

    Twenty-fourth

---
+++ level24/classwork/cw1.py
@@ -0,0 +1,26 @@
+# 1) გააკეთეთ სარეგისტრაციო, მომხმარებელს შემოატანიეთ სახელი და პაროლი, 
+# რეგისტრაციის შემდეგ მოხმარებელი უნდა შევიდეს ექაუნთზე, შეეკითხეთ 
+# სახელი და პაროლი მომხარებელს რათა შევიდეს ექაუნთზე სანამ მომხმარებელი არ შეიტანს 
+# იმ ინფორმაციას რაც შეიყვანა რეგისტრაციის დროს მანამ დაიბეჭდოს რომ
+#  შეტანილი ინფორმაცია არასწორია და შეეკითხოს თავიდან, ხოლო 
+# თუ მომხმარებელმა ყველაფერი სწორად შეიყვანა დაიბეჭდოს welcome
+
+
+
+
+user_name = input("enter your name here: ")
+
+
+password = (input("enter password here broo: "))
+
+
+while user_name != "emzara":
+    print("your name is incorrect!")
+    user_name = input("please enter name!")
+    
+while  password != "good22":
+    print("enter real passwoo")
+
+
+print("welcome")
+    

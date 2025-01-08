edb868067d2020faf0d641d492b05801cab9804c
JUJI18 <tornikekhurcia5@gmail.com>
2025-01-08 02:06:53 -0800

    Twenty-Ninth commit

--- level26/homework/hw5.py
+++ level26/homework/hw5.py
@@ -8,6 +8,6 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 num= 0
 
 
-for num in my_list:
+for num in (my_list):
     if num %3 == 0:
         print(num)
---
+++ level28/homework/hw3.py
@@ -0,0 +1,19 @@
+#3)შექმენით სია, რომელიც შეიცავს 6 რიცხვს. გამოიტანეთ სიის ელემენტების ჯამი და საშუალო მნიშვნელობა.
+
+
+
+
+my_numbers = [22, 34, 31, 20, 49, 56]
+
+
+total =(my_numbers[0] + my_numbers[1] + my_numbers[2] + my_numbers[3] + my_numbers[4] + my_numbers[5])
+
+average = total / 6
+
+print(total)
+
+print(average)
+
+
+
+
